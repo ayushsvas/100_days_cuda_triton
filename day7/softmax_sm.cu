d766fa24e9b06539533baf0004038abaf8e2e8c4
ayushsvas <diracmanifold@gmail.com>
2025-04-22 18:05:02 +0200

    day7:softmax with shared memory, revision

---
+++ day7/softmax_sm.cu
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <math.h>
+
+// __device__ int min(float , float b){
+//     if (a < b){
+//         return a;
+//     }
+//     return b;
+// }
+
+__global__ void softmax(float *a, float *b, const int size){
+    int tid = threadIdx.x;
+    const int numThreads = blockDim.x;
+    const int NUM_THREADS = 2;
+    if (tid >= size) return ;
+
+    int num_elements_per_thread = size / blockDim.x;
+    int startidx = tid*num_elements_per_thread;
+    int endidx = min(size, startidx+num_elements_per_thread);
+
+    
+
+    __shared__ float maxi[NUM_THREADS];
+    __shared__ float sums[NUM_THREADS];
+    
+    float max = 0.0;
+    for (int i = startidx; i<endidx; ++i){
+        if (a[i] > max){
+            max = a[i];
+        }
+    }
+    maxi[tid] = max;
+    max = 0.0;
+    for (int i=0; i< numThreads;++i){
+        if (maxi[i]>max){
+            max = maxi[i];
+        }
+    }
+
+    float total = 0.0;
+    for (int i = startidx; i<endidx; ++i){
+        total = total + expf(a[i]-max);
+    }
+    sums[tid] = total;
+
+    total = 0.0;
+    for (int i = 0; i < numThreads; ++i){
+        total = total + sums[i];
+    }
+
+    for (int i=startidx; i<endidx; ++i){
+        b[i] = expf(a[i]-max)/total;
+    }
+
+
+}
+
+
+
+int main(){
+    const int size = 8;
+
+    float a[size];
+    for (int i = 0; i<size; ++i){
+        a[i] = i;
+    }
+
+    float *d_a, *d_b; 
+    cudaMalloc(&d_a, size*sizeof(float));
+    cudaMalloc(&d_b, size*sizeof(float));
+
+    cudaMemcpy(d_a, a, size*sizeof(float), cudaMemcpyHostToDevice);
+
+    int num_ThreadsPerBlock = 2;
+    int num_ThreadBlocks = 1;
+
+
+    softmax <<< num_ThreadBlocks, num_ThreadsPerBlock >>> (d_a, d_b, size);
+
+    cudaMemcpy(a, d_b, size*sizeof(float), cudaMemcpyDeviceToHost);
+
+    std::cout<<"Probabilites are..."<<std::endl;
+
+    for (int i =0; i< size;++i){
+        std::cout<<a[i]<<" ";
+    }
+    cudaFree(d_a);
+    cudaFree(d_b);
+    std::cout << "\n";
+    
+}
