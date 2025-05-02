d767198cb7bd7160c395b9a5911e08d42f5a69f7
ayushsvas <diracmanifold@gmail.com>
2025-05-02 10:45:53 +0200

    day17:sec 3.3, 3.4 error macros, constant memory

---
+++ day17/error_macro.cu
@@ -0,0 +1,40 @@
+#include <cuda_runtime.h>
+
+# define ERROR_CHECK(call){\
+    cudaError_t err = call; \
+    if (err != cudaSuccess){ \
+    printf("%s in %s at line %d \n", \
+        cudaGetErrorString(err), __FILE__, __LINE__);\
+    exit(EXIT_FAILURE);\
+    }\
+} 
+
+
+
+
+// Error checking macro
+#define CHECK_CUDA(call) { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        fprintf(stderr, "CUDA error in line %d: %s\n", __LINE__, \
+                cudaGetErrorString(err)); \
+        return err; \
+    } \
+}
+
+#define CHECK_CUFFT(call) { \
+    cufftResult_t err = call; \
+    if (err != CUFFT_SUCCESS) { \
+        fprintf(stderr, "CUFFT error in line %d: %d\n", __LINE__, err); \
+        return cudaErrorUnknown; \
+    } \
+}
+
+#define CHECK_CUBLAS(call)                                                      \
+    do {                                                                        \
+        cublasStatus_t status = call;                                           \
+        if (status != CUBLAS_STATUS_SUCCESS) {                                  \
+            std::cerr << "cuBLAS error\n";                                      \
+            exit(EXIT_FAILURE);                                                 \
+        }                                                                       \
+    } while (0)
