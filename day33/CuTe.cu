69174ceeffad1a9eae7612810926bb8a32bd250b
ayushsvas <diracmanifold@gmail.com>
2025-05-18 23:04:43 +0200

    day33:cuTe, flash attention contd.

---
+++ day33/CuTe.cu
@@ -0,0 +1,3 @@
+//learnt a bit of CuTe from the latest CuTe lecture on GPU Mode
+//Read the flash attention paper, a bit more. Didn't understand the read/write operations 
+// being O(Nd+N^2) for the normal attention and O(N^2d^2M^-1).
